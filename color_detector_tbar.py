6fbce368ad5b108d714321a57694513e38515c46
player-119c <112776797+player-119c@users.noreply.github.com>
2023-09-06 11:44:47 +0530

    Update color_detector_tbar.py

--- color_detector_tbar.py
+++ color_detector_tbar.py
@@ -47,3 +47,11 @@ while True:
 
 cap.release()
 cv.destroyAllWindows()
+
+#for light blue (90,150,50,128,50,255)
+#for blue (81,133,167,255,81,255)
+#for red (0,6,141,255,99,255)
+#for orange and yellow (5,24,37,255,124,255)
+#for darkgreen (29,85,69,255,105,255)
+#for pink and purple (28,174,176,255,120,255) val  min low
+#for black (0,179,0,255,0,32)
